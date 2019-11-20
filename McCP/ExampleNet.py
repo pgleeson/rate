aa98f214355b249dfa654791b22fe41a8754c60f
Padraig Gleeson <p.gleeson@gmail.com>
2019-11-20 13:50:45 +0000

    Tweaks

--- McCP/ExampleNet.py
+++ McCP/ExampleNet.py
@@ -20,7 +20,7 @@ def generate():
     
 
     net.parameters = { 'amp': 1.5,
-                       'scale': 4}
+                       'scale': 10}
 
     cell = Cell(id='mccp0', lems_source_file='McCPTest.xml')
     net.cells.append(cell)
@@ -55,7 +55,7 @@ def generate():
                                       synapse=rsDL.id,
                                       pre_synapse=silentDL.id,
                                       type='continuousProjection',
-                                      weight=0.5,
+                                      weight='random()',
                                       random_connectivity=RandomConnectivity(probability=0.6)))
 
     '''
@@ -75,16 +75,22 @@ def generate():
                                       weight='in_weight',
                                       random_connectivity=RandomConnectivity(probability=0.7)))'''
 
-    input_source = InputSource(id='sg0', neuroml2_source_file='inputs.nml')
-    net.input_sources.append(input_source)
+    input_source0 = InputSource(id='sg0', neuroml2_source_file='inputs.nml')
+    net.input_sources.append(input_source0)
+    input_source1 = InputSource(id='sg1', neuroml2_source_file='inputs.nml')
+    net.input_sources.append(input_source1)
 
     for pop in [p0.id]:
-        net.inputs.append(Input(id='stim_%s'%pop,
-                                input_source=input_source.id,
+        net.inputs.append(Input(id='stim0_%s'%pop,
+                                input_source=input_source0.id,
                                 population=pop,
-                                percentage=100))
-
-    
+                                percentage=60))
+                                
+        net.inputs.append(Input(id='stim1_%s'%pop,
+                                input_source=input_source1.id,
+                                population=pop,
+                                percentage=60))
+                                
 
     #print(net)
     #print(net.to_json())
@@ -109,112 +115,13 @@ def generate():
 
 if __name__ == "__main__":
 
-    if '-sweep' in sys.argv:
-        
-        sim, net = generate()
-        sim.recordTraces={}
-        
-        fixed = {'dt':0.025, 'order':5}
- 
-        vary = {'eta':[0.5,1,1.5,2,3,4,5,6,7,8,9,10]}
-        #vary = {'eta':[1,2,5]}
-        vary['seed'] = [i for i in range(10)]
-        vary['seed'] = [i for i in range(5)]
 
-        simulator = 'jNeuroML'
-        simulator = 'PyNN_NEST'
-        simulator = 'jNeuroML_NetPyNE'
-        simulator = 'jNeuroML_NEURON'
-        simulator = 'jNeuroML'
+    sim, net = generate()
 
-        nmllr = NeuroMLliteRunner('SimExample7.json',
-                                  simulator=simulator)
+    ################################################################################
+    ###   Run in some simulators
 
-        ps = ParameterSweep(nmllr, 
-                            vary, 
-                            fixed,
-                            num_parallel_runs=16,
-                            plot_all=False, 
-                            heatmap_all=False,
-                            show_plot_already=False,
-                            peak_threshold=0)
+    import sys
 
-        report = ps.run()
-        ps.print_report()
-
-        #  ps.plotLines('weightInput','average_last_1percent',save_figure_to='average_last_1percent.png')
-        #ps.plotLines('weightInput','mean_spike_frequency',save_figure_to='mean_spike_frequency.png')
-        #ps.plotLines('eta','Einput[0]/spike:mean_spike_frequency',save_figure_to='mean_spike_frequency.png')
-        ps.plotLines('eta','expoisson/0/poisson_input/spike:mean_spike_frequency',second_param='seed',save_figure_to='mean_spike_frequency_ein.png')
-        ps.plotLines('eta','inpoisson/0/poisson_input/spike:mean_spike_frequency',second_param='seed',save_figure_to='mean_spike_frequency_iin.png')
-        ps.plotLines('eta','Epop/0/ifcell/spike:mean_spike_frequency',second_param='seed',save_figure_to='mean_spike_frequency_e.png')
-        ps.plotLines('eta','Ipop/0/ifcell/spike:mean_spike_frequency',second_param='seed',save_figure_to='mean_spike_frequency_i.png')
-
-        import matplotlib.pyplot as plt
-
-        plt.show()
-        
-    if '-sweep2' in sys.argv:
-        
-        sim, net = generate()
-        sim.recordTraces={}
-        
-        fixed = {'dt':0.025, 'order':5}
- 
-        vary = {'eta':[0.5,1,1.5,2,3]}
-        vary = {'epsilon':[0.01,0.1,0.2,0.5,0.9]}
-        vary = {'J':[0.01,0.1,0.2,0.5,0.9]}
-        #vary = {'g':[0.5,1,1.5,2,3,20]}
-        
-        first = vary.keys()[0]
-        
-        #vary = {'eta':[1,2,5]}
-        #vary['seed'] = [i for i in range(10)]
-        vary['seed'] = [i for i in range(3)]
-
-        simulator = 'jNeuroML'
-        simulator = 'PyNN_NEST'
-        simulator = 'jNeuroML_NetPyNE'
-        simulator = 'jNeuroML'
-        simulator = 'jNeuroML_NEURON'
-
-        nmllr = NeuroMLliteRunner('SimExample7.json',
-                                  simulator=simulator)
-
-        ps = ParameterSweep(nmllr, 
-                            vary, 
-                            fixed,
-                            num_parallel_runs=16,
-                            plot_all=False, 
-                            heatmap_all=False,
-                            show_plot_already=False,
-                            peak_threshold=0)
-
-        report = ps.run()
-        ps.print_report()
-
-        #  ps.plotLines('weightInput','average_last_1percent',save_figure_to='average_last_1percent.png')
-        #ps.plotLines('weightInput','mean_spike_frequency',save_figure_to='mean_spike_frequency.png')
-        #ps.plotLines('eta','Einput[0]/spike:mean_spike_frequency',save_figure_to='mean_spike_frequency.png')
-        
-        second = 'seed'
-        ps.plotLines(first,'expoisson/0/poisson_input/spike:mean_spike_frequency',second_param=second,save_figure_to='mean_spike_frequency_ein.png')
-        ps.plotLines(first,'inpoisson/0/poisson_input/spike:mean_spike_frequency',second_param=second,save_figure_to='mean_spike_frequency_iin.png')
-        ps.plotLines(first,'Epop/0/ifcell/spike:mean_spike_frequency',second_param=second,save_figure_to='mean_spike_frequency_e.png')
-        ps.plotLines(first,'Ipop/0/ifcell/spike:mean_spike_frequency',second_param=second,save_figure_to='mean_spike_frequency_i.png')
-
-        import matplotlib.pyplot as plt
-
-        plt.show()
-    
-    else:
-
-        sim, net = generate()
-
-        ################################################################################
-        ###   Run in some simulators
-
-        import sys
-
-        check_to_generate_or_run(sys.argv, sim)
+    check_to_generate_or_run(sys.argv, sim)
 
